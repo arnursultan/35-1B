be2a1adc999224739d6a48bd6f706c5fffc4c731
arnursultan <badasspubgm>
2026-05-05 16:42:28 +0600

    Lesson 2 Custom Permissions up

--- products/models.py
+++ products/models.py
@@ -14,10 +14,10 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TexField(blank=True)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    category = models.ForeingKey(
+    category = models.ForeignKey(
         Category, on_delete=models.SET_NULL,
         null=True, blank=True, related_name="products"
     )
--- products/views.py
+++ products/views.py
@@ -42,5 +42,85 @@ class CategoryDetailView(APIView):
         category = self.get_object(pk)
         if not category:
             return Response({"error": "Категория не найдена"}, status= status.HTTP_404_NOT_FOUND)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
 
-        
+    def put(self, request, pk):
+        category = self.get_object(pk)
+        if not category:
+            return Response({"error": "Категория не найдена"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = CategorySerializer(category, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        return Response({"error": "Категория не наайдена"}, status=status.HTTP_404_NOT_FOUND)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ProductListCreateView(APIView):
+    permissions_classes = [IsAdminOrManagerOrReadOnly]
+
+    def get(self, request):
+        products = Product.objects.filter(is_active=True).select_related("owner", "category")
+        serializer = ProductSerializer(products, many=True, context={"request": request})
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data, context={"request": request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class ProductDetailView(APIView):
+    def get_permissions(self):
+        if self.request.method in ("PATCH", "PUT"):
+            return [IsOwnerOrAdmin()]
+        if self.request.method == "DELETE":
+            return [IsAdminOrManager()]
+        return [IsAuthenticated()]
+
+    def get_object(self, pk, request):
+        try:
+            obj = Product.objects.get(pk=pk)
+            self.check_object_permissions(request, obj)
+            return obj
+        except Product.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        product = Product.objects.filter(pk=pk).select_related("owner", "category").first()
+        if not product:
+            return Response({"error": "Продукт не найден"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(product, context={"request": request})
+        return Response(serializer.data)
+
+    def patch(self, request,pk):
+        product = self.get_object(pk, request)
+        if not product:
+            return Response({"error": "Продукт не найден"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(
+            product, data=request.data, partial=True, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request,pk):
+        product = self.get_object(pk, request)
+        if not product:
+            return Response({"error": "Продукт не найден"}, status=status.HTTP_404_NOT_FOUND)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class MyProductView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        products = Product.objects.all(
+            owner=request.user
+        ).select_related("category")
+        serializer = ProductSerializer(products, many=True, context={"request": request})
+        return Response(serializer.data)
